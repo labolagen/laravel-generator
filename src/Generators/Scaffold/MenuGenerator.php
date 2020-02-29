ef6c4b129036d8ff5e6f4b451ead87cc16d7023a
skys215 <skys215@gmail.com>
2020-02-29 22:59:12 +0800

    Fixed bug, menu generator path problem.

--- src/Generators/Scaffold/MenuGenerator.php
+++ src/Generators/Scaffold/MenuGenerator.php
@@ -12,10 +12,13 @@ class MenuGenerator extends BaseGenerator
     private $commandData;
 
     /** @var string */
-    private $path;
+    private $menuBladePath;
 
     /** @var string */
-    private $filename;
+    private $menuFilePath;
+
+    /** @var string */
+    private $fileName;
 
     /** @var string */
     private $templateType;
@@ -32,7 +35,7 @@ class MenuGenerator extends BaseGenerator
     public function __construct(CommandData $commandData)
     {
         $this->commandData = $commandData;
-        $this->path = config(
+        $basepath = config(
             'infyom.laravel_generator.path.views',
             resource_path('views/'
             )
@@ -44,55 +47,55 @@ class MenuGenerator extends BaseGenerator
         if ($this->commandData->isLocalizedTemplates()) {
             $templateName .= '_locale';
         }
-        $this->filename = $this->commandData->config->mPlural.'.blade.php';
 
         $this->menuTemplate = get_template('scaffold.layouts.'.$templateName, $this->templateType);
 
-        $this->path .= $commandData->getAddOn('menu.menu_file');
+        $this->menuFilePath = $basepath.$this->commandData->getAddOn('menu.menus_folder');
+        $this->menuBladePath = $basepath.$this->commandData->getAddOn('menu.menu_file');
+        $this->fileName = $this->commandData->config->mPlural.'.blade.php';
+
         $this->menuContents = fill_template($this->commandData->dynamicVars, $this->menuTemplate);
         $this->menuIncludeContent = '@include(\''.str_replace('/','.',$commandData->getAddOn('menu.menu_file')).$this->commandData->config->mHumanPlural.'\')';
     }
 
     public function generate()
     {
-        $menuFilePath = $this->path.$this->commandData->getAddOn('menu.menus_folder');
-        if (file_exists($menuFilePath.$this->fileName) && $this->confirm("\nDo you want to overwrite ".$this->commandData->config->mPlural." menu? [y|N]", false)) {
+        if (file_exists($this->menuFilePath.$this->fileName) && $this->confirm("\nDo you want to overwrite ".$this->commandData->config->mPlural." menu? [y|N]", false)) {
             $this->commandData->commandObj->info('Menu '.$this->commandData->config->mPlural.' already exists, Skipping Adjustment.');
 
             return;
         }
 
-        FileUtil::createFile($this->path, $this->fileName, $this->menuContents);
+        FileUtil::createFile($this->menuFilePath, $this->fileName, $this->menuContents);
         $this->commandData->commandComment("\nAdded ".$this->commandData->config->mCamelPlural.' to menu.');
 
         $this->insertIntoMenuBlade();
     }
 
     public function insertIntoMenuBlade(){
-        $menuBladePath = $this->path.$this->commandData->getAddOn('menu.menu_file');
-        $existingMenuContents = file_get_contents($menuBladePath);
+        $existingMenuContents = file_get_contents($this->menuBladePath);
         if (Str::contains($existingMenuContents, $this->menuIncludeContent)) {
             $this->commandData->commandObj->info('Menu '.$this->commandData->config->mHumanPlural.' already exists in menu.blade, Skipping Adjustment.');
 
             return;
         }
 
-        file_put_contents($menuBladePath, $existingMenuContents."\n".$this->menuIncludeContent);
+        file_put_contents($this->menuBladePath, $existingMenuContents."\n".$this->menuIncludeContent);
         $this->commandData->commandComment("\n".$this->commandData->config->mCamelPlural.' menu added to menu.blade.');
 
     }
 
     public function rollback()
     {
-        $existingMenuContents = file_get_contents($menuBladePath);
+        $existingMenuContents = file_get_contents($this->menuBladePath);
         if (Str::contains($this->menuContents, $this->menuTemplate)) {
             file_put_contents($this->path, str_replace($this->menuIncludeContent, '', $existingMenuContents));
             $this->commandData->commandComment('menu deleted');
         }
 
-        if(file_exists($this->filename)){
-            unlink($this->filename);
-            $this->commandData->commandObj->info('Deleted '.$this->filename.'.');
+        if(file_exists($this->menuFilePath.$this->fileName)){
+            unlink($this->menuFilePath.$this->fileName);
+            $this->commandData->commandObj->info('Deleted '.$this->fileName.'.');
         }
     }
 }
