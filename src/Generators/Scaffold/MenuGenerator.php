96a2708896d15cf23024b61b37582ef8e9cd1455
skys215 <skys215@gmail.com>
2020-02-29 21:20:46 +0800

    Fixed menu generator bug, added rollback

--- src/Generators/Scaffold/MenuGenerator.php
+++ src/Generators/Scaffold/MenuGenerator.php
@@ -15,6 +15,9 @@ class MenuGenerator extends BaseGenerator
     private $path;
 
     /** @var string */
+    private $filename;
+
+    /** @var string */
     private $templateType;
 
     /** @var string */
@@ -22,6 +25,9 @@ class MenuGenerator extends BaseGenerator
 
     /** @var string */
     private $menuTemplate;
+
+    /** @var string */
+    private $menuIncludeContent;
 
     public function __construct(CommandData $commandData)
     {
@@ -38,24 +44,25 @@ class MenuGenerator extends BaseGenerator
         if ($this->commandData->isLocalizedTemplates()) {
             $templateName .= '_locale';
         }
+        $this->filename = $this->commandData->config->mPlural.'.blade.php';
 
         $this->menuTemplate = get_template('scaffold.layouts.'.$templateName, $this->templateType);
 
         $this->path .= $commandData->getAddOn('menu.menu_file');
         $this->menuContents = fill_template($this->commandData->dynamicVars, $this->menuTemplate);
+        $this->menuIncludeContent = '@include(\''.str_replce('/','.',$commandData->getAddOn('menu.menu_file')).$this->commandData->config->mHumanPlural.'\')';
     }
 
     public function generate()
     {
         $menuFilePath = $this->path.$commandData->getAddOn('menu.menus_folder');
-        $fileName = $this->commandData->config->mPlural.'.blade.php';
-        if (file_exists($menuFilePath.$fileName) && $this->confirm("\nDo you want to overwrite ".$this->commandData->config->mPlural." menu? [y|N]", false)) {
+        if (file_exists($menuFilePath.$this->fileName) && $this->confirm("\nDo you want to overwrite ".$this->commandData->config->mPlural." menu? [y|N]", false)) {
             $this->commandData->commandObj->info('Menu '.$this->commandData->config->mPlural.' already exists, Skipping Adjustment.');
 
             return;
         }
 
-        FileUtil::createFile($this->path, $fileName, $this->routesTemplate);
+        FileUtil::createFile($this->path, $this->fileName, $this->menuContents);
         $this->commandData->commandComment("\nAdded ".$this->commandData->config->mCamelPlural.' to menu.');
 
         $this->insertIntoMenuBlade();
@@ -64,23 +71,28 @@ class MenuGenerator extends BaseGenerator
     public function insertIntoMenuBlade(){
         $menuBladePath = $this->path.$commandData->getAddOn('menu.menu_file');
         $existingMenuContents = file_get_contents($menuBladePath);
-        $menuIncludeContent = '@include(\''.str_replce('/','.',$commandData->getAddOn('menu.menu_file')).$this->commandData->config->mHumanPlural.'\')';
-        if (Str::contains($existingMenuContents, $menuIncludeContent)) {
+        if (Str::contains($existingMenuContents, $this->menuIncludeContent)) {
             $this->commandData->commandObj->info('Menu '.$this->commandData->config->mHumanPlural.' already exists in menu.blade, Skipping Adjustment.');
 
             return;
         }
 
-        file_put_contents($menuBladePath, $existingMenuContents."\n".$menuIncludeContent);
+        file_put_contents($menuBladePath, $existingMenuContents."\n".$this->menuIncludeContent);
         $this->commandData->commandComment("\n".$this->commandData->config->mCamelPlural.' menu added to menu.blade.');
 
     }
 
     public function rollback()
     {
+        $existingMenuContents = file_get_contents($menuBladePath);
         if (Str::contains($this->menuContents, $this->menuTemplate)) {
-            file_put_contents($this->path, str_replace($this->menuTemplate, '', $this->menuContents));
+            file_put_contents($this->path, str_replace($this->menuIncludeContent, '', $existingMenuContents));
             $this->commandData->commandComment('menu deleted');
+        }
+
+        if(file_exists($this->filename)){
+            unlink($this->filename);
+            $this->commandData->commandObj->info('Deleted '.$this->filename.'.');
         }
     }
 }
